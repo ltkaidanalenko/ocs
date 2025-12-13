08f0f7857169851ae17b354d97d43d619f2614bb
ltkaidanalenko <acostarafaeljb@gmail.com>
2025-12-13 07:54:56 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const info = {
+title:'how rare and beautiful it truly is that we exist',
+text:'Lorem ipsum dolor sit amet.'
+header:'https://placehold.co/1920x300/222/eee.png',
+icon:'https://placehold.co/100/222/eee.png'
+};
+
+const characters = {
+ john_doe: {
+  name: 'John Doe',
+  universe: 'Cyberpunk 2077'
+ },
+ jane_smith: {
+  name: 'Jane Smith',
+  universe: 'Baldur\'s Gate 3'
+ },
+};
