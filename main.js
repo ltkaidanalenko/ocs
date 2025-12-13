71f6c1f47726a8090a75d4c9b1845692ed350b2a
ltkaidanalenko <acostarafaeljb@gmail.com>
2025-12-13 08:12:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,15 +10,21 @@ $(function(){
 const characters = {
   john_doe: {
    name: 'John Doe',
-   universe: 'Cyberpunk 2077'
+   universe: 'Cyberpunk 2077',
+   image:'https://placehold.co/150x200/222/eee.png',
   },
   jane_smith: {
    name: 'Jane Smith',
-   universe: 'Baldur\'s Gate 3'
+   universe: 'Baldur\'s Gate 3',
+   image:'https://placehold.co/150x200/222/eee.png',
   },
  };
 
  $('profile header').append('<img src="'+info.header+'"/>');
  $('profile side').append('<img src="'+info.icon+'"/><h1>'+info.title+'</h1><p>'+info.text+'</p><ul></ul>');
+
+ for (const oc in characters) {
+   $('profile main').append(`<oc id="${oc}"><img src="${characters[oc].image}"/><h2>${characters[oc].name}</h2><h3>${characters[oc].universe}</h3></oc>`);
+ }
  
 });
